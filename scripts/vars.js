89d51e6bc17bcb736b156b87b8e7db3e2cd1d3b5
UrkoU <47209630+Deagle50@users.noreply.github.com>
2022-05-02 19:30:11 +0200

    Detección automática de ip

--- scripts/vars.js
+++ scripts/vars.js
@@ -1,9 +1,14 @@
-const url = "http://148.3.87.123:6745/";
+var url = "http://192.168.0.200:6745/";
+
+$.getJSON("https://ipinfo.io/json?token=9853e4ad6cf8ae", function (data) {
+  url = data.ip == "148.3.87.123" ? "http://192.168.0.200:6745/" : "http://148.3.87.123:6745/";
+});
+
 // const url = "http://10.10.17.164:3000/";
 let token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaGVjayI6dHJ1ZSwiaWF0IjoxNjUxNDk1NDAxLCJleHAiOjE2NTE1ODIwNjF9.ZT5AAKz9kQ9FU8BI3syIUgPrMrqU6VEFHn1k2CFcm8c";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaGVjayI6dHJ1ZSwiaWF0IjoxNjUxNTEwNDY1LCJleHAiOjE2NTE1OTcxMjV9.uGTGjegExKYaiFyf8swuotRmKiM0y0pIIa4fsULjqxE";
 
-var settings = {
+let settings = {
   url: url,
   method: "GET",
   timeout: 0,
